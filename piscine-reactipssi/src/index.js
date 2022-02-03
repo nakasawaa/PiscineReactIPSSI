e643902742c80899db20ffd0c4d64cde66a219db
nakasawaa <c.cancino@ecole-ipssi.net>
2022-02-03 15:05:03 +0100

    Création des pages + routes

--- piscine-reactipssi/src/index.js
+++ piscine-reactipssi/src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import AppNavbar from './Components/AppNavbar/AppNavbar'
+import HomePage from './Pages/HomePage/HomePage'
+import CarnetPage from './Pages/CarnetPage'
+import CarnetAddPage from './Pages/CarnetAddPage'
+import NotesPreviewPage from './Pages/NotesPreviewPage'
+import NotesAddPage from './Pages/NotesAddPage'
+import NotesUpdatePage from './Pages/NotesUpdatePage'
+import ConfigPage from './Pages/ConfigPage'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <main>
+      <Router>
+        <header className="mb-5">
+          <AppNavbar></AppNavbar>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/carnet" element={<CarnetPage />} />
+          <Route path="/carnet/add" element={<CarnetAddPage />} />
+          <Route path="/notes/preview" element={<NotesPreviewPage />} />
+          <Route path="/notes/add" element={<NotesAddPage />} />
+          <Route path="/notes/:id" element={<NotesUpdatePage />} />
+          <Route path="/config" element={<ConfigPage />} />
+        </Routes>
+      </Router>
+    </main>
   </React.StrictMode>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
