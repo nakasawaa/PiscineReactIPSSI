1937be081b94d756c886b4044e675da98ae67ceb
nakasawaa <c.cancino@ecole-ipssi.net>
2022-02-03 16:22:11 +0100

    Correction de pb

---
+++ piscine-reactipssi/src/Components/AppNavbar/AppNavbar.jsx
@@ -0,0 +1,23 @@
+import { Navbar, Container, Nav } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+
+export default function AppNavbar() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Piscine IPSSI</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">
+              Accueil
+            </Nav.Link>
+            <Nav.Link as={Link} to="/carnet">
+              Carnets
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
--- piscine-reactipssi/src/index.js
+++ piscine-reactipssi/src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import AppNavbar from './Components/AppNavbar/AppNavbar'
-import HomePage from './Pages/HomePage/HomePage'
+import HomePage from './Pages/HomePage'
 import CarnetPage from './Pages/CarnetPage'
 import CarnetAddPage from './Pages/CarnetAddPage'
 import NotesPreviewPage from './Pages/NotesPreviewPage'
@@ -16,18 +16,18 @@ ReactDOM.render(
   <React.StrictMode>
     <main>
       <Router>
-        <header className="mb-5">
+        <header >
           <AppNavbar></AppNavbar>
         </header>
 
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          {/* <Route path="/" element={<HomePage />} />
           <Route path="/carnet" element={<CarnetPage />} />
           <Route path="/carnet/add" element={<CarnetAddPage />} />
           <Route path="/notes/preview" element={<NotesPreviewPage />} />
           <Route path="/notes/add" element={<NotesAddPage />} />
           <Route path="/notes/:id" element={<NotesUpdatePage />} />
-          <Route path="/config" element={<ConfigPage />} />
+          <Route path="/config" element={<ConfigPage />} /> */}
         </Routes>
       </Router>
     </main>
